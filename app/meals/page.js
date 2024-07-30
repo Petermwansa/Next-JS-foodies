39e3303a3962eff6e0bdf9f413fa4b5b32d58469
Petermwansa <mwansapeter800@gmail.com>
2024-07-30 11:29:48 +0300

    Add the meals functionality but to be completed later

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,22 @@
 import Link from "next/link";
+import classes from './page.module.css';
 
 export default function MealsPage() {
     return (
-        <main>
-            <h1>The Meals</h1>
-            <p><Link href='/meals/meal-1'>Meal 1</Link></p>
-            <p><Link href='/meals/meal-2'>Meal 2</Link></p>
-        </main>
+        <>
+            <header className={classes.header}></header>
+            <main className={classes.main}>
+                <h1>
+                    Delicious meals, created{' '}
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>Choose your favourite recipe and cook it yourself. It is easy and fun</p>
+                <p className={classes.cta}>
+                    <Link href='/meals/share'>
+                        Share your favourite recipe 
+                    </Link>
+                </p>
+            </main>
+        </>
     )
 }
