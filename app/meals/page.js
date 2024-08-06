6e7e70b50fccf3964794cb52fae0c32a2db18afb
Petermwansa <mwansapeter800@gmail.com>
2024-08-06 12:26:10 +0300

    Add the meal item component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import classes from './page.module.css';
+import MealsGrid from "@/components/meals/meals-grid";
 
 export default function MealsPage() {
     return (
         <>
-            <header className={classes.header}></header>
-            <main className={classes.main}>
+            <header className={classes.header}>
                 <h1>
                     Delicious meals, created{' '}
                     <span className={classes.highlight}>by you</span>
@@ -13,9 +13,12 @@ export default function MealsPage() {
                 <p>Choose your favourite recipe and cook it yourself. It is easy and fun</p>
                 <p className={classes.cta}>
                     <Link href='/meals/share'>
-                        Share your favourite recipe 
+                        Share your favourite recipe
                     </Link>
                 </p>
+            </header>
+            <main className={classes.main}>
+               <MealsGrid meals={[]}/> 
             </main>
         </>
     )
--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -1,4 +1,3 @@
-
 "use client"
 
 import Link from "next/link";
@@ -12,6 +11,11 @@ export default function NavLink({ href, children}) {
     const path = usePathname();
 
     return (
-        <Link href={href} className={path.startsWith(href) ? `${classes.link} ${classes.active}` : classes.link }>{children}</Link>
+        <Link 
+            href={href}
+            className={path.startsWith(href) ? `${classes.link} ${classes.active}` : classes.link }
+        >
+        {children}
+        </Link>
     )
 }
